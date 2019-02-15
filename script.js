0a48337bfac937f3bfd66e0b0a5e024be528088f
Guilherme Klai <gklaidossantos@gmail.com>
2019-02-15 15:39:50 -0200

    created escadinha

---
+++ script.js
@@ -0,0 +1,10 @@
+let algarismos = (prompt("Qual algarismo você que usar?"))
+
+let escadinha = algarismos;
+
+let tamanho = Number(prompt("Qual o numero de degrais que você quer?"));
+
+ for (let i=1; i<= tamanho; i++){
+    console.log (algarismos);
+    algarismos = algarismos + escadinha;
+ }
